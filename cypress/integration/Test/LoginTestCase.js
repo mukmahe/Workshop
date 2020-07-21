4e4e0134f9efe4253ed4c31a088689a6e9dd511d
Mukul Maheshwari <mukulmaheshwari@Mukuls-MacBook-Pro.local>
2020-07-21 12:26:12 +0530

    Level2-Adding fixtures and Env Variables

--- cypress/integration/Test/LoginTestCase.js
+++ cypress/integration/Test/LoginTestCase.js
@@ -2,25 +2,24 @@
 
 //Adding Fixtures and Env Variables
 describe('Validating Successful Login ', function () {  //This is our test Suite
-
     beforeEach('Visit URL', function () {   //Like testNG before Method. Runs before every test case
         cy.visit(Cypress.env("url"))
+        cy.fixture("example.json").then(function (data) {
+            this.data = data
+        })
     })
 
     it("Enter Incorrect email id and password", function () {
-        cy.fixture("example.json").then(function (data) {
-            cy.get("input[id='usr']").type(data.incorrectName)
-            cy.get("input[id='pwd']").type(data.incorrectPass)
-            cy.get("[value='Login']").click()
-            cy.get("#case_login>h3").should("have.text", data.failureMessage)
-        })
+        cy.get("input[id='usr']").type(this.data.incorrectName)
+        cy.get("input[id='pwd']").type(this.data.incorrectPass)
+        cy.get("[value='Login']").click()
+        cy.get("#case_login>h3").should("have.text", this.data.failureMessage)
     })
+
     it("Enter Correct email id and password", function () {
-        cy.fixture("example.json").then(function (data) {
-            cy.get("input[id='usr']").type(data.correctName)
-            cy.get("input[id='pwd']").type(data.correctPass)
-            cy.get("[value='Login']").click()
-            cy.get("#case_login>h3").should("have.text", data.successMessage)
-        })
+        cy.get("input[id='usr']").type(this.data.correctName)
+        cy.get("input[id='pwd']").type(this.data.correctPass)
+        cy.get("[value='Login']").click()
+        cy.get("#case_login>h3").should("have.text", this.data.successMessage)
     })
 })
