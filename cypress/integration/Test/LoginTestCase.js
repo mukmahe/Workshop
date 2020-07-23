d1dde9265081c150877d9372168f77b3ce7b8ebf
Mukul Maheshwari <mukulmaheshwari@Mukuls-MacBook-Pro.local>
2020-07-23 23:40:14 +0530

    Level1- Add products to cart and verify count

--- cypress/integration/Test/LoginTestCase.js
+++ cypress/integration/Test/LoginTestCase.js
@@ -1,22 +1,32 @@
 /// <reference types="Cypress" />    //Added for auto suggestions
 
-describe('Validating Successful Login ', function () {  //This is our test Suite
+describe('Validating Successful Addition to Cart', function () {  //This is our test Suite
 
-    beforeEach('Visit URL', function () {   //Like testNG before Method. Runs before every test case
-        cy.visit("http://testing-ground.scraping.pro/login")  //cy is just like driver. It is a global command which helps us to invoke any cypress command
+    before('Visit URL and Login', function () {   //Like testNG before Method. Runs before every test case
+        cy.visit("https://spree-vapasi.herokuapp.com/login")  //cy is just like driver. It is a global command which helps us to invoke any cypress command
         //We need not make object of cy. It's globally available
+        cy.get("input[id='spree_user_email']").type("mukul@gmail.com")     //You can also use wildcards
+        cy.get("input[id='spree_user_password']").type("123456")
+        cy.get("[value='Login']").click()
+        //We should never make one test dependent on other test
     })
 
-    it("Enter Incorrect email id and password", function () {  //First Test Case
-        cy.get("input[id='usr']").type("abc")     //You can also use wildcards
-        cy.get("input[id='pwd']").type("abc")
-        cy.get("[value='Login']").click()
-        cy.get("#case_login>h3").should("have.text", "ACCESS DENIED!") //Can be done using Promise Handling also
+    it("Select Product Category as Bag and name as Ruby on Rails and add product to cart", function () {  //First Test Case
+        cy.contains("Bags").click()
+        cy.get("[title='Ruby on Rails Tote']").click()
+        //Now here we are just verifying if Cart API is giving us 200 or not? If yes then proceed
+        cy.server()
+        cy.route("/cart_link").as("cart")
+        cy.get("#add-to-cart-button").click()
+        cy.wait("@cart").then(function (response) {
+            expect(response.status).equals(200)
+        })
+        cy.get("table[id='cart-detail']>tbody[id='line_items']>tr").should("have.length",1)
     })
-    it("Enter Correct email id and password", function () {  //First Test Case
-        cy.get("input[id='usr']").type("admin")     //You can also use wildcards
-        cy.get("input[id='pwd']").type("12345")
-        cy.get("[value='Login']").click()
-        cy.get("#case_login>h3").should("have.text", "WELCOME :)")
+
+    it("Select Product Category as Mugs and name as Ruby on Rails Mug and add Product to Cart", function () {
+        cy.get("#home-link").click()
+
+        //Assignment --
     })
 })
