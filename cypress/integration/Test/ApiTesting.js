604d2fd0fb5cb46ae007c921b33ef7123382d3bf
Mukul Maheshwari <muk.mahe@gmail.com>
2020-09-29 01:18:51 +0530

    Initial Level2 commit

--- cypress/integration/Test/ApiTesting.js
+++ cypress/integration/Test/ApiTesting.js
@@ -13,22 +13,21 @@ describe("Verify API waits and Stubs",function () {
         })
     })
 
-    it("Verify if XHR Wait is working for POST",function () {
-        /*
-        ---------- Assignment
-         */
-    })
-
     it("Verify if Mocks is working for PUT",function () {
         cy.server();
         cy.route({method:"PUT", url:"comments/1",status: 404, response: { error: "My customized message" }, delay: 500}).as("PUTCall");
         cy.get("[class*='network-put']").first().click();
     })
 
-    it.skip("Verify if Mock is working for GET call",function () {
-        /*
-        -------- Assignment
-        Instead of comment on GET mock your name
-         */
+    it("Verify if Mock is working for GET call",function () {
+        cy.server();
+        cy.route({method: "GET", url:"comments/1", status: 200, response: {
+                "postId": 1,
+                "id": 1,
+                "name": "id labore ex et quam laborum",
+                "email": "Mukul@gardner.biz",
+                "body": "Muku;"
+            },delay: 500}).as("GET")
+        cy.get(".network-btn").first().click()
     })
 })
