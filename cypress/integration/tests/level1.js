6aadd26d54ba017472e284e4ff414127e3276acd
Mukul Maheshwari <muk.mahe@gmail.com>
2020-10-03 23:54:33 +0530

    Added solution to the assignment problem

--- cypress/integration/tests/level1.js
+++ cypress/integration/tests/level1.js
@@ -2,13 +2,12 @@
 
 describe('Validating Successful Appointment of the User', function () {  //This is our test Suite
 
-    before('Visit URL and Login', function () {   //Like testNG before Method. Runs before every test case
+    before('Visit URL and Login', function () {   //Like testNG beforeTest. Runs once before 1st it block
         cy.visit("https://katalon-demo-cura.herokuapp.com/profile.php#login");  //cy is just like driver.
         // It is a global command which helps us to invoke any cypress command
-        //We need not make object of cy. It's globally available
     })
 
-    beforeEach("Verify Login", function () {
+    beforeEach("Verify Login", function () { //Like testNG beforeMethod. Runs before every it block
         //We should never make one test dependent on other test
         cy.get("#btn-make-appointment").click();
         cy.get("[id='txt-username']").type("John Doe");
@@ -24,22 +23,33 @@ describe('Validating Successful Appointment of the User', function () {  //This 
         //Handle Calender to enter 28-Sep-2020
         cy.get("#txt_visit_date").click();
         cy.get("[class='datepicker-switch']").first().click();
-        cy.contains("Sep").click({force:true});
-        cy.contains("28").click({force:true});
-
+        cy.contains("Sep").click({force: true});
+        cy.contains("28").click({force: true});
         cy.get("#txt_comment").type("Test Comment")
         cy.get("#btn-book-appointment").click();
-        cy.get("#facility").should("have.text","Hongkong CURA Healthcare Center")
-        cy.get("section[id='summary'] p[class='lead']").then(function (ele) {
-            expect(ele.text().toString()).includes("appointment has been booked");
-        })
+        cy.get("#facility").should("have.text", "Hongkong CURA Healthcare Center")
+        cy.get("section[id='summary'] p[class='lead']").should("include.text", "appointment has been booked");
     })
 
-    it.skip("Book appointment for Seoul CURA Healthcare Center", function () {
+    it("Book appointment for Seoul CURA Healthcare Center", function () {
         /*
         ------ Assignment
 
          */
+
+        //Solution
+        cy.get("#combo_facility").select("Seoul CURA Healthcare Center");
+        cy.get("#chk_hospotal_readmission").click();
+        cy.get("[value='Medicaid']").click();
+        //Handle Calender to enter 28-Sep-2020
+        cy.get("#txt_visit_date").click();
+        cy.get("[class='datepicker-switch']").first().click();
+        cy.contains("Sep").click({force: true});
+        cy.contains("28").click({force: true});
+        cy.get("#txt_comment").type("Test Comment")
+        cy.get("#btn-book-appointment").click();
+        cy.get("#facility").should("have.text", "Seoul CURA Healthcare Center")
+        cy.get("section[id='summary'] p[class='lead']").should("include.text", "appointment has been booked");
     })
 
     afterEach("Verify Logout Functionality", function () {
