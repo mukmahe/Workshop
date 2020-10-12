56975ea7513c0a5a0c5e54fd9985de04a35f9769
Mukul Maheshwari <muk.mahe@gmail.com>
2020-10-12 22:06:21 +0530

    Changed website during workshop

--- cypress/integration/tests/promise_handling.js
+++ cypress/integration/tests/promise_handling.js
@@ -5,7 +5,16 @@ describe("Demo promise handling", function () {
         cy.visit("https://katalon-demo-cura.herokuapp.com")
         /*
         const webElement=cy.get("#btn-make-appointment")
+        //Not allowed since cy.get is cypress command and const webElement is javascript command
         const text=webElement.text()
          */
+        //Hence to to mitigage it you have to use
+        cy.get("#btn-make-appointment").then(function (ele) {
+            const text = ele.text()
+            cy.log(text)
+            /* Refer PPT for more links
+            Over here we are asking cypress to find element and then only execute the commands inside
+            cypress is async at its heart, hence if you want to mix js code and cypress code, you will have to resolve promise*/
+        })
     })
 })
--- cypress/integration/tests/reporting.js
+++ cypress/integration/tests/reporting.js
@@ -1,37 +1,43 @@
 /// <reference types="Cypress" />
 
-const LoginPage = require("../../pages/login_page")
-const AppointmentPage = require("../../pages/appointment_page")
-const ConfirmPage = require("../../pages/confirm_page")
-const LogoutPage = require("../../pages/logout_page")
-const hospitalData = require("../../fixtures/hospital_data.json")
+const landingPage = require("../../pages/landing_page")  //require is just like imports in java
+const addComputer = require("../../pages/add_computer")
+const computerData = require("../../fixtures/computer_data.json") //Fixtures- Test Data
+describe('Validating Computer Database Application', function () {
 
-describe('Validating Successful AppointmentPage of the User', function () {
-
-    before('Visit URL and Login', function () {
+    before('Visit URL', function () {
         cy.visit(Cypress.env('url'))
     })
-
-    beforeEach("Verify Login", function () {
-        cy.fixture("login_data").then(function (login) {
-            LoginPage.login(login.username, login.password)
-            cy.url().should("contain", "#appointment")
-        })
+    //Added pages landingPage and addComputer in pages folder
+    //Added .json file, which contains all the test data
+    it("Add new Computer in the Database", function () {
+        landingPage.clickAddBtn()
+        addComputer.addComputer(computerData.computerName, computerData.date, computerData.company)
+        addComputer.confirmMessage()
     })
 
-    it("Book appointment for Hongkong CURA Healthcare Center", function () {
-        AppointmentPage.bookAppointment(hospitalData.Hospital1, hospitalData.readmission, hospitalData.program, hospitalData.visitDate, hospitalData.comment)
-        ConfirmPage.confirmFacilityName(hospitalData.Hospital1)
-        ConfirmPage.confirmText()
-    })
-
-    it("Book appointment for Seoul CURA Healthcare Center", function () {
-        AppointmentPage.bookAppointment(hospitalData.Hospital2, hospitalData.readmission, hospitalData.program, hospitalData.visitDate, hospitalData.comment)
-        ConfirmPage.confirmFacilityName(hospitalData.Hospital2)
-        ConfirmPage.confirmText()
-    })
-
-    afterEach("Verify Logout Functionality", function () {
-        LogoutPage.logout()
+    it("Search for a computer name", function () {
+        landingPage.filterComputer(computerData.searchComputer)
+        landingPage.verifySearchResult(computerData.searchComputer)
     })
 })
+
+/*
+Refer PPT for more links
+To add reporting feature in your project
+Step 1: npm install --save-dev mochawesome
+Step 2: Add this file in your cypress.json file
+{
+  "reporter": "mochawesome",
+  "reporterOptions": {
+    "reportDir": "cypress/results",
+    "overwrite": false,
+    "html": false,
+    "json": true
+  }
+}
+Step 3: Just simply run your tests in run mode
+npx cypress run
+
+ */
+
